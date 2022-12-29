27132b79c1565b85f7f8264176cad7c5a8cdaf5f
Dominique <dominique.del1801@alumnos.ubiobio.cl>
2022-12-29 15:49:40 -0300

    cambio variable

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,7 +1,7 @@
 const variables = {
   host:
   process.env.NODE_ENV === "production"
-  ? "https://evaluacionaccesible.netlify.app"
+  ? "https://proyectotitulo.herokuapp.com"
   : "http://127.0.0.1:64578",
      
 };
