2ce14d7218cf8732e48630c5cc06ddd336a74994
Dominique <dominique.del1801@alumnos.ubiobio.cl>
2022-12-29 15:40:55 -0300

    prod

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,8 +1,8 @@
 const variables = {
   host:
   process.env.NODE_ENV === "production"
-  ? "http://127.0.0.1:64578"
-  : "https://evaluacionaccesible.netlify.app",
+  ? "https://evaluacionaccesible.netlify.app"
+  : "http://127.0.0.1:64578",
      
 };
 
