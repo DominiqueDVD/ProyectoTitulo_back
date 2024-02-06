65da7531840130eeee721c11c1c13e63f1d77d52
Dominique Del Valle Durán <157436090+DominiqueDVD@users.noreply.github.com>
2024-02-05 23:13:42 -0300

    Update variables.js2

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,7 +1,7 @@
 const variables = {
   host:
   process.env.NODE_ENV === "production"
-  ? "https://proyecto-titulo-back.vercel.app/"
+  ? "https://proyecto-titulo-back.vercel.app"
   : "http://127.0.0.1:64578",
      
 };
